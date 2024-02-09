93115351ce26dad28b7ea99880ea433a4f14ad4a
yevhensnik <yevhengren@gmail.com>
2024-02-09 17:14:50 +0200

    code clean

--- src/main/java/com/test/redis/springbootredis/SpringBootRedisApplication.java
+++ src/main/java/com/test/redis/springbootredis/SpringBootRedisApplication.java
@@ -3,8 +3,6 @@ package com.test.redis.springbootredis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.test.redis.springbootredis.*")
